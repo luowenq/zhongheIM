967f7dc0f9251b48b5509c85f4405388a947ba39
HJQ <15983272836@163.com>
2019-12-24 12:45:34 +0800

    提交

--- app/src/main/java/com/exam/fs/push/utils/ToastUtils.java
+++ app/src/main/java/com/exam/fs/push/utils/ToastUtils.java
@@ -1,9 +1,7 @@
 package com.exam.fs.push.utils;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.text.TextUtils;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Toast;
 
